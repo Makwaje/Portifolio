5d22abf675e644782378930b5ddb30dceb145fec
makwaje <alloushi2010@gmail.com>
2024-01-10 04:51:11 +0200

    react email disable

--- src/email/contact-form-email.tsx
+++ src/email/contact-form-email.tsx
@@ -1,42 +1,42 @@
-import {
-  Html,
-  Body,
-  Head,
-  Heading,
-  Hr,
-  Container,
-  Preview,
-  Section,
-  Text,
-} from "react-email/components";
-import { Tailwind } from "@react-email/tailwind";
+// import {
+//   Html,
+//   Body,
+//   Head,
+//   Heading,
+//   Hr,
+//   Container,
+//   Preview,
+//   Section,
+//   Text,
+// } from "react-email/components";
+// import { Tailwind } from "@react-email/tailwind";
 
-type ContactFormEmailProps = {
-  message: string;
-  senderEmail: string;
-};
+// type ContactFormEmailProps = {
+//   message: string;
+//   senderEmail: string;
+// };
 
-function ContactFormEmail({ message, senderEmail }: ContactFormEmailProps) {
-  return (
-    <Html>
-      <Head />
-      <Preview>New message from your portfolio site</Preview>
-      <Tailwind>
-        <Body className="bg-gray-100 text-black">
-          <Container>
-            <Section className="borderBlack my-10 rounded-md bg-white px-10 py-4 ">
-              <Heading className="leading-tight">
-                You received the following message from the contact form
-              </Heading>
-              <Text>{message}</Text>
-              <Hr />
-              <Text>The sender email is: {senderEmail}</Text>
-            </Section>
-          </Container>
-        </Body>
-      </Tailwind>
-    </Html>
-  );
-}
+// function ContactFormEmail({ message, senderEmail }: ContactFormEmailProps) {
+//   return (
+//     <Html>
+//       <Head />
+//       <Preview>New message from your portfolio site</Preview>
+//       <Tailwind>
+//         <Body className="bg-gray-100 text-black">
+//           <Container>
+//             <Section className="borderBlack my-10 rounded-md bg-white px-10 py-4 ">
+//               <Heading className="leading-tight">
+//                 You received the following message from the contact form
+//               </Heading>
+//               <Text>{message}</Text>
+//               <Hr />
+//               <Text>The sender email is: {senderEmail}</Text>
+//             </Section>
+//           </Container>
+//         </Body>
+//       </Tailwind>
+//     </Html>
+//   );
+// }
 
-export default ContactFormEmail;
+// export default ContactFormEmail;
