27db19271d7329b675438d16b1041181be4c9d11
Mohamed Bakri <98839008+Makwaje@users.noreply.github.com>
2024-02-01 12:47:44 +0200

    Update meta data desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Mohamed Bakri | Personal Portfolio",
   description:
-    "Mohamed Bakri is full-stack developer with 8 years of experience",
+    "Mohamed Bakri is full-stack developer with 3 years of experience",
 };
 
 export default function RootLayout({
